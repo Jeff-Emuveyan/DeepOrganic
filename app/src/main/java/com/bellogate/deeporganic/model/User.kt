2e557eeef62a7d58f404226b13e1ae04d6ab52fb
jeffemuveyan <jemuveyan@seamfix.com>
2021-01-10 15:24:36 +0100

    Initial UI for account.

--- app/src/main/java/com/bellogate/deeporganic/model/User.kt
+++ app/src/main/java/com/bellogate/deeporganic/model/User.kt
@@ -1,3 +1,3 @@
 package com.bellogate.deeporganic.model
 
-class User(var name: String, var email: String, var profilePictureURL: String)
+class User(var name: String, var email: String, var profilePictureURL: String, var numberOfFollowers: Int)
