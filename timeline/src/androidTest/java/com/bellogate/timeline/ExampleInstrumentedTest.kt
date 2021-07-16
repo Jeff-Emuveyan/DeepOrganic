5b38322aa4cb758f99aa7259f54b6bbec04e6b7d
jeffemuveyan <jemuveyan@seamfix.com>
2021-07-16 23:25:13 +0100

    update

--- timeline/src/androidTest/java/com/bellogate/timeline/ExampleInstrumentedTest.kt
+++ timeline/src/androidTest/java/com/bellogate/timeline/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.seamfix.timeline.test", appContext.packageName)
+        assertEquals("com.bellogate.timeline.test", appContext.packageName)
     }
 }
